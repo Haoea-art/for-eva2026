08b8a5107bea9fac09517bcb07001d539e975ad4
Haoea-art <llllyyyy020820@gmail.com>
2025-12-31 17:17:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
+  plugins: [react()],
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
   }
 });
